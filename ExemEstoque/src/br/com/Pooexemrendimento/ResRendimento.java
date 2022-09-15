04c7eb12b32b998d29ea1f3f19eb8fe45372c64d
TalisonMoura <111541453+TalisonMoura@users.noreply.github.com>
2022-09-14 22:51:31 -0300

    Add files via upload

---
+++ ExemEstoque/src/br/com/Pooexemrendimento/ResRendimento.java
@@ -0,0 +1,11 @@
+package br.com.Pooexemrendimento;
+
+public class ResRendimento {
+
+	public static void main(String[] args) {
+		Rendimento rend1 = new Rendimento(1000,2,12);
+			
+		System.out.println(rend1.RendMes());	
+	}
+
+}
